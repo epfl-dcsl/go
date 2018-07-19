bcf82b79a6669a7afa3e9575751b2f954fa7f53c
Adrien Ghosn <ghosn.adrien@gmail.com>
2018-07-19 14:09:09 +0200

    Skipping the readlinkat

--- src/syscall/gosec.go
+++ src/syscall/gosec.go
@@ -43,6 +43,63 @@ UNSUPPORTED:
 }
 
 func Syscall6(trap, a1, a2, a3, a4, a5, a6 uintptr) (r1, r2 uintptr, err Errno) {
-	//TODO interpose
+	//if runtime.IsEnclave() {
+	//	switch trap {
+	//	case SYS_READLINKAT:
+	//		syscid, csys := runtime.Cooprt.AcquireSysPool()
+	//		allocid, cal := runtime.Cooprt.AcquireAllocPool()
+
+	//		// Compute the size of the string.
+	//		size := uintptr(0)
+	//		for {
+	//			c := (*byte)(unsafe.Pointer(a2 + size))
+	//			if *c == byte(0) {
+	//				break
+	//			}
+	//			size++
+	//		}
+	//		size++ // account for null terminator
+
+	//		//Copy the content of the string outside of the enclave.
+	//		runtime.Cooprt.OAllocReq <- runtime.AllocAttr{int(size), nil, allocid}
+	//		bufS := <-cal
+	//		size = uintptr(0)
+	//		for i := 0; i < len(bufS.Buf); i++ {
+	//			val := (*byte)(unsafe.Pointer(a2 + size))
+	//			bufS.Buf[i] = *val
+	//			size++
+	//		}
+
+	//		//Allocate the buffer outside of the enclave.
+	//		var na3 uintptr
+	//		var bufU *runtime.AllocAttr
+	//		if int(a4) > 0 {
+	//			runtime.Cooprt.OAllocReq <- runtime.AllocAttr{int(a4), nil, allocid}
+	//			bufU = <-cal
+	//			na3 = uintptr(unsafe.Pointer(&bufU.Buf[0]))
+	//		} else {
+	//			na3 = uintptr(unsafe.Pointer(&_zero))
+	//		}
+
+	//		runtime.Cooprt.ReleaseAllocPool(allocid)
+
+	//		na2 := uintptr(unsafe.Pointer(&bufS.Buf[0]))
+
+	//		// Make the call.
+	//		runtime.Cooprt.Ocall <- runtime.OcallReq{true, trap, a1, na2, na3, a4, a5, a6, syscid}
+	//		res := <-csys
+	//		runtime.Cooprt.ReleaseSysPool(syscid)
+
+	//		//Copy back the buffer.
+	//		if int(a4) > 0 {
+	//			memcpy(a3, na3, a4)
+	//		}
+	//		return res.R1, res.R2, Errno(res.Err)
+
+	//	default:
+	//		goto UNSUPPORTED
+	//	}
+	//}
+	//UNSUPPORTED:
 	return SSyscall6(trap, a1, a2, a3, a4, a5, a6)
 }
