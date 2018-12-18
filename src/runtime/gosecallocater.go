ae59cb6dbab0cd9d3bbbab3ba90d2ca14eda7c87
Adrien Ghosn <ghosn.adrien@gmail.com>
2018-12-18 11:24:27 +0100

    Cleaning and refactoring

--- src/runtime/gosecallocater.go
+++ src/runtime/gosecallocater.go
@@ -1,7 +1,6 @@
 package runtime
 
 import (
-	"runtime/internal/atomic"
 	"unsafe"
 )
 
@@ -52,31 +51,6 @@ type sgentry struct {
 type spanlist struct {
 	head uspanptr
 	tail uspanptr
-}
-
-type slock struct {
-	key uint32
-}
-
-//go:nosplit
-func (l *slock) lock() {
-	for {
-		for i := 0; i < active_spin_cnt; i++ {
-			if l.key == mutex_unlocked {
-				if atomic.Cas(&l.key, mutex_unlocked, mutex_locked) {
-					return
-				}
-			}
-		}
-		procyield(15)
-	}
-}
-
-//go:nosplit
-func (l *slock) unlock() {
-	if v := atomic.Xchg(&l.key, mutex_unlocked); v != mutex_locked {
-		throw("Error on spinlock.")
-	}
 }
 
 type uledger struct {
